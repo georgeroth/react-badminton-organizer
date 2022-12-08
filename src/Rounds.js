dcf60417ded7c41b1820f3657d3e35dd195df675
George Roth <g@georgeroth.eu>
2022-12-08 10:54:12 +0000

    Rounds page JSX done

--- src/Rounds.js
+++ src/Rounds.js
@@ -1,7 +1,74 @@
 export default function Setup() {
     return (
         <main>
-            <div>This is the rounds page</div>
+            <h1>
+                Rounds
+            </h1>
+            <table className="results-table">
+                <tr className="table-header">
+                    <td className="player"></td>
+                    <td>Round 1</td>
+                    <td>Round 2</td>
+                    <td>Round 3</td>
+                    <td>Round 4</td>
+                    <td>Round 5</td>
+                    <td>Round 6</td>
+                </tr>
+                <tr className="table-content">
+                    <td className="player">Player Name 1</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                </tr>
+                <tr className="table-content">
+                    <td className="player">Player Name 2</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                </tr>
+                <tr className="table-content">
+                    <td className="player">Player Name 3</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                </tr>
+                <tr className="table-content">
+                    <td className="player">Player Name 4</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x"></td>
+                </tr>
+                <tr className="table-content">
+                    <td className="player">Player Name 5</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                </tr>
+                <tr className="table-content">
+                    <td className="player">Player Name 6</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                    <td className="match-x">X</td>
+                    <td className="match-x"></td>
+                </tr>
+            </table>
         </main>
     )
 }
